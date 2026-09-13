20a455cd676690d865219e8558390c5f763d1fdd
Isabella Brito <isadocarmo7@gmail.com>
2026-09-12 21:43:24 -0300

    feat: adiciona parte de contatos e footer

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -5,10 +5,12 @@ import { GithubService } from '../../services/github';
 import { Repository } from '../../types/repository.interface';
 import { About } from '../../shared/about/about';
 import { Projects } from '../../shared/projects/projects';
+import { Contact } from '../../shared/contact/contact';
+import { Footer } from '../../shared/footer/footer';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, MainSection, About, Projects],
+  imports: [Header, MainSection, About, Projects, Contact, Footer],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
