d155ea9dbfdaba42e3400d068c9dd5a2cbe7f6bd
Isabella Brito <isadocarmo7@gmail.com>
2026-09-13 12:42:38 -0300

    feat: adicionando badge flutuante de premiações nos cards dos projetos vencedores

--- src/app/types/project_extra.interface.ts
+++ src/app/types/project_extra.interface.ts
@@ -5,7 +5,10 @@ export interface ProjectExtra {
 }
 
 export const PROJECT_EXTRAS: Record<string, ProjectExtra> = {
-  'nome-do-repo': {
-    screenshot: 'assets/projects',
-  }
+  'challenge-letssign-frontend': {
+    achievement: '🏆 1º Lugar — NEXT FIAP 2025',
+  },
+  'challenge-wtc': {
+    achievement: 'Finalista — NEXT FIAP 2026',
+  },
 }
