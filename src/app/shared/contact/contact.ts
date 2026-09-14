a9b156082605a88362b1b55f0e32531359a86bec
Isabella Brito <isadocarmo7@gmail.com>
2026-09-13 23:30:13 -0300

    chore: preenchendo mini bio e sobre mim.

--- src/app/shared/contact/contact.ts
+++ src/app/shared/contact/contact.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { BtnPrimary } from '../btn-primary/btn-primary';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [BtnPrimary],
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
